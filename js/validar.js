e6d41d319d30836198c26e6b840f438e16f24d08
Juan Fabian Pincheira Campos <vec_jpincheira@clinicalascondes.cl>
2021-06-04 14:04:54 -0400

    Proyecto 2.0 ESCALAB

--- js/validar.js
+++ js/validar.js
@@ -1,31 +1,69 @@
 
 function validacion(){
-    var formvalido = true;
-
+    
+    let datos = new Object();
     var nombre = document.getElementById("nombre").value;
     var email = document.getElementById("email").value;
     var telefono = document.getElementById("telefono").value;
     var direccion = document.getElementById("direccion").value;
+    var descripcion = document.getElementById("descripcion").value;
 
 
-    if(nombre == null || nombre.length == 0){
-        alert("elnombre no puede estar vacio ");
-        formvalido = false;
+       // OBTENGO NOMBRE Y EVALUO
+    if(nombre.length < 5){
+        document.getElementById("msgNombre").innerText = "Ingrese un nombre correcto...";
+
+    }else{
+        document.getElementById("msgNombre").innerText = "";
+        datos["Nombre"] = nombre;
     }
+    //OBTENGO NOMBRE EVALUO, CARACTERES, LETRAS Y TIPOLOGIA 
 
-    if(email == null || email.length == 0){
-        alert("el email no puede estar vacio ");
-        formvalido = false;
-    }
-    if(telefono == null || telefono.length == 0){
-        alert("el telefono no puede estar vacio ");
-        formvalido = false;
-    }
-    if(direccion == null || direccion.length == 0){
-        alert("la direccion no puede estar vacio ");
-        formvalido = false;
-    }
+    if(email.length > 0){
+        var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if(!regex.test(email)){
+            document.getElementById("msgCorreo").innerText = "Ingrese correo valido";
+            }else{
+                document.getElementById("msgCorreo").innerText = "";
+                datos["Email"] = email;
+            }
+            
+         }else{
+            document.getElementById("msgCorreo").innerText = "Obligatorio correo";
 
+         }
 
-    return formvalido;
-}
+            //EVALUO SI EL TELEFONO ES CORRECTO
+            if(telefono.length < 10){
+                document.getElementById("msgTelefono").innerText = "El telefono no es valido";
+                } else{
+                    document.getElementById("msgTelefono").innerText = "";  
+                    datos["Telefono"] = telefono;
+            }
+        
+            //EVALUO SI LA DIRECCION ES CORRECTO
+            if(direccion == 0){
+                document.getElementById("msgDireccion").innerText = "Debe señlecionar una opcion";
+            }else{
+                document.getElementById("msgDireccion").innerText = "";  
+                datos["Direccion"] = direccion;
+            }
+
+            //EVALUO SI LA DESCRIPCION ES CORRECTA
+
+            if(descripcion.length <=0){
+                document.getElementById("msgDescripcion").innerText = "Escribir mas 10 caractares";
+                
+            }else{
+                document.getElementById("msgDescripcion").innerText = "";
+                datos["Descripcion"] = descripcion;
+            }
+            console.log(datos);
+        } 
+
+        
+     
+     
+
+  
+
